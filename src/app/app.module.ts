22aa3fdddebb593e8ba7ef6feb7f1180b66a5750
Suman <sumanguha1989@gmail.com>
2021-09-11 01:03:56 +0530

    core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { DomainComponent } from './domain/domain.component';
 import { SubDomainComponent } from './sub-domain/sub-domain.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { NavComponent } from './core/nav/nav.component';
+import { ContactUsComponent } from './core/contact-us/contact-us.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       DomainComponent,
       SubDomainComponent,
-      HomeComponent
+      HomeComponent,
+      HeaderComponent,
+      NavComponent,
+      ContactUsComponent,
+      FooterComponent
    ],
    imports: [
 	 BrowserModule,

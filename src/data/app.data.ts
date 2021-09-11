8489919686fb2c14100f70b79cdbd8871c8932e7
Suman <sumanguha1989@gmail.com>
2021-09-11 10:58:56 +0530

    domain details data

--- src/data/app.data.ts
+++ src/data/app.data.ts
@@ -32,43 +32,176 @@ export const AppData = {
       imageUrl: 'assets/images/mobileapp.png',
       heading: 'Mobile Application Development',
       description: 'Cross platform, reliable and performant mobile applications',
-      name: 'mobile-app'
+      name: 'mobile-app',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
+            }
+        ]
+      }
     },
     {
       imageUrl: 'assets/images/desktopapp.png',
       heading: 'Desktop Application Development',
       description: 'Efficient, Reliable, Standalone and Crossplatform desktop applications',
-      name: 'desktop-app'
+      name: 'desktop-app',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
+            }
+        ]
+      }
     },
     {
       imageUrl: 'assets/images/cloudapp.png',
       heading: 'Cloud Services',
       description: 'Cloud Hosting, Cloud Migration, Analytics and a lot more',
-      name: 'cloud'
+      name: 'cloud',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
+            }
+        ]
+      }
     },
     {
       imageUrl: 'assets/images/consulting.png',
       heading: 'Consulting',
       description: 'Effective, Realistic and Optimum software consultancy',
-      name: 'consulting'
+      name: 'consulting',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
+            }
+        ]
+      }
     },
     {
       imageUrl: 'assets/images/iot.png',
       heading: 'IOT Integration',
       description: 'IOT Software solutions, Edge computing, Analytics and a lot more',
-      name: 'iot'
+      name: 'iot',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
+            }
+        ]
+      }
     },
     {
       imageUrl: 'assets/images/automation.png',
       heading: 'Process Automation',
       description: 'Process Analysis, Optimization, DevOps and Automation services',
-      name: 'automation'
+      name: 'automation',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
+            }
+        ]
+      }
     },
     {
       imageUrl: 'assets/images/qa.png',
       heading: 'Quality Assurance',
       description: 'Software Testing, Quality Analysis, Improvement Planning and lot more',
-      name: 'qa'
+      name: 'qa',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
+            }
+        ]
+      }
     }
   ]
 }

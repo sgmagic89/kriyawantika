1039d75a7e0e66b879c67449fa85744d930d80bc
Suman <sumanguha1989@gmail.com>
2021-09-11 10:48:09 +0530

    domain details

--- src/data/app.data.ts
+++ src/data/app.data.ts
@@ -1,59 +1,74 @@
 export const AppData = {
-    mainBannerData: {
-        companyName: 'Kriyawantika Solutions',
-        tagLine: 'We provide Enterprise level software development and support services'
-    },
-    domainData: [
-        { 
-            imageUrl: 'assets/images/webapp.png',
-            heading: 'Web Application Development',
-            description: 'UI Design, UX and Web Development with latest tools and technology',
-            name: 'web-app',
-            domainDetails: {
-
+  mainBannerData: {
+    companyName: 'Kriyawantika Solutions',
+    tagLine: 'We provide Enterprise level software development and support services'
+  },
+  domainData: [{
+      imageUrl: 'assets/images/webapp.png',
+      heading: 'Web Application Development',
+      description: 'UI Design, UX and Web Development with latest tools and technology',
+      name: 'web-app',
+      domainDetails: {
+        capabilities: `A web application (or web app) is application software that runs on a web server, unlike computer-based 
+        software programs that are run locally on the operating system (OS) of the device. Web applications are accessed by 
+        the user through a web browser with an active network connection. There are lots of technologies available to develop such 
+        apps but as its very volatile technology stack we keep our selfs up to date with the latest technology trends and tools to 
+        provide you the best web applications to make your vision reachable to billions of people over internet.`,
+        details: [
+            { 
+                heading: 'How we do it',
+                description: 'Test Description',
+                imageUrl: 'assets/images/howwedo.jpg',
+            },
+            { 
+                heading: 'Technologies & Tools',
+                description: 'Test Description',
+                imageUrl: 'assets/images/techandtools.png',
             }
-        },
-        { 
-            imageUrl: 'assets/images/mobileapp.png',
-            heading: 'Mobile Application Development',
-            description: 'Cross platform, reliable and performant application development',
-            name: 'mobile-app'
-        },
-        { 
-            imageUrl: 'assets/images/desktopapp.png',
-            heading: 'Desktop Application Development',
-            description: 'Efficient, Reliable, Standalone and Crossplatform desktop applications',
-            name: 'desktop-app'
-        },
-        { 
-            imageUrl: 'assets/images/cloudapp.png',
-            heading: 'Cloud Services',
-            description: 'Cloud Hosting, Cloud Migration, Analytics and a lot more',
-            name: 'cloud'
-        },
-        { 
-            imageUrl: 'assets/images/consulting.png',
-            heading: 'Consulting',
-            description: 'Effective, Realistic and Optimum software consultancy',
-            name: 'consulting'
-        },
-        { 
-            imageUrl: 'assets/images/iot.png',
-            heading: 'IOT Integration',
-            description: 'IOT Software solutions, Edge computing, Analytics and a lot more',
-            name: 'iot'
-        },
-        { 
-            imageUrl: 'assets/images/automation.png',
-            heading: 'Process Automation',
-            description: 'Process Analysis, Optimization, DevOps and Automation services',
-            name: 'automation'
-        },
-        { 
-            imageUrl: 'assets/images/qa.png',
-            heading: 'Quality Assurance',
-            description: 'Software Testing, Quality Analysis, Improvement Planning and lot more',
-            name: 'qa'
-        }
-    ]
- }
+        ]
+      }
+    },
+    {
+      imageUrl: 'assets/images/mobileapp.png',
+      heading: 'Mobile Application Development',
+      description: 'Cross platform, reliable and performant mobile applications',
+      name: 'mobile-app'
+    },
+    {
+      imageUrl: 'assets/images/desktopapp.png',
+      heading: 'Desktop Application Development',
+      description: 'Efficient, Reliable, Standalone and Crossplatform desktop applications',
+      name: 'desktop-app'
+    },
+    {
+      imageUrl: 'assets/images/cloudapp.png',
+      heading: 'Cloud Services',
+      description: 'Cloud Hosting, Cloud Migration, Analytics and a lot more',
+      name: 'cloud'
+    },
+    {
+      imageUrl: 'assets/images/consulting.png',
+      heading: 'Consulting',
+      description: 'Effective, Realistic and Optimum software consultancy',
+      name: 'consulting'
+    },
+    {
+      imageUrl: 'assets/images/iot.png',
+      heading: 'IOT Integration',
+      description: 'IOT Software solutions, Edge computing, Analytics and a lot more',
+      name: 'iot'
+    },
+    {
+      imageUrl: 'assets/images/automation.png',
+      heading: 'Process Automation',
+      description: 'Process Analysis, Optimization, DevOps and Automation services',
+      name: 'automation'
+    },
+    {
+      imageUrl: 'assets/images/qa.png',
+      heading: 'Quality Assurance',
+      description: 'Software Testing, Quality Analysis, Improvement Planning and lot more',
+      name: 'qa'
+    }
+  ]
+}
